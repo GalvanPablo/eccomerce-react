4e73568312f9bb96632932c753bdf93235581481
Pablo Galvan <pablogalvan.015@gmail.com>
2022-12-20 18:47:07 -0300

    Inicialización y configuración

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+        <BeakerIcon className="w-6 h-6 text-blue-500" />
+      </h1>
+    </>
+  );
+}
+
+export default App;
